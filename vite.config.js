407c40f913d5927aa464da791f74d9fa6944ab32
Chisrus <kouakoucoffichrisemmanuelk@gmail.com>
2026-03-06 02:17:20 +0000

    Add /admin route redirecting to admin access

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,8 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
+        admin: resolve(__dirname, 'admin/index.html'),
+        adminAccess: resolve(__dirname, 'admin-access.html'),
         login: resolve(__dirname, 'login.html'),
         register: resolve(__dirname, 'register.html'),
         marketplace: resolve(__dirname, 'marketplace.html'),
